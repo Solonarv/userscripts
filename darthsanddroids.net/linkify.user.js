5c6e70023a8e951c2a8130676fea4511ab2580ba
Solonarv <nstamm@gmx.de>
2017-01-20 00:19:47 +0100

    Create linkify.user.js

---
+++ darthsanddroids.net/linkify.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Darths and Droids Linkifier
+// @namespace    http://darthsanddroids.net
+// @version      0.1
+// @author       Solonarv
+// @match        http://darthsanddroids.net/episodes/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var imgs = document.getElementsByTagName("img");
+    var comicImage;
+    for (var i=0; i < imgs.length; i++){
+        if (/.*\/comics\/.*/.test(imgs[i].src)){
+            comicImage = imgs[i];
+            break;
+        }
+    }
+    if (typeof comicImage === "undefined") return;
+    console.log("found comic image!");
+    var comicImageParent = comicImage.parentElement;
+
+    var links = document.getElementsByTagName("a");
+    var nextLink;
+    for (var j=0; j < links.length; j++){
+        if (links[j].innerText === "NEXT>"){
+            nextLink = links[j];
+            break;
+        }
+    }
+
+    if (typeof nextLink === "undefined") return;
+    console.log("found next link!");
+
+    var imgWrapper = document.createElement("a");
+    imgWrapper.href = nextLink.href;
+    comicImageParent.insertBefore(imgWrapper, comicImage);
+    comicImageParent.removeChild(comicImage);
+    imgWrapper.appendChild(comicImage);
+})();
